391b5382332920131800c83d804bd6c29fffa6f3
Xevione <bturcat@gaming.tech>
2023-10-30 19:58:21 +0100

    main

--- 2048 SDL/main.cpp
+++ 2048 SDL/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 #include <SDL.h>
 
-int main(int argc, char** args) {
+int main(int argc, char* argv[])
+{
+	int windowSize = 720;
+	int statut = EXIT_FAILURE;
+	bool Game = true;
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	SDL_Event event;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "Erreur SDL_INIT_VIDEO : %s", SDL_GetError());
+		goto Quit;
+	}
 
-	return 0;
+	SDL_RaiseWindow(window);
+	SDL_Rect test;
+	test.x = 520;
+	test.y = 520;
+	SDL_RenderFillRect(renderer, &test);
+
+
+	window = SDL_CreateWindow("2048", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		windowSize, windowSize, SDL_WINDOW_SHOWN);
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_Delay(5000);
+
+
+
+Quit:
+	if (NULL != renderer)
+		SDL_DestroyRenderer(renderer);
+	if (NULL != window)
+		SDL_DestroyWindow(window);
+	SDL_Quit();
+	return statut;
+	
 }
